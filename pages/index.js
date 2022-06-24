284fb6d5d98563225f7a6bd6df3453cfb5e530a5
Ale <alearanibar@gmail.com>
2022-06-23 23:03:35 -0700

    project complete

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log("results", results);
   return (
     <div>
       <Head>
@@ -13,6 +16,20 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `${requests[genre].url || requests.fetchTrending.url}`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
